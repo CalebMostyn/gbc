962c0fbf61d6bb0278aa7b891ebad6f07b467e9e
CalebMostyn <mostyncr@miamioh.edu>
2025-04-30 09:53:22 -0400

    Added Register File with Register Pairs

--- include/cpu.h
+++ include/cpu.h
@@ -1,6 +1,28 @@
 #ifndef CPU_H
 #define CPU_H
 
+#include <stdint.h>
+
 void clock_cpu(); // emulate a clock tick on the cpu
+
+typedef union {
+    struct {
+        uint8_t l;
+        uint8_t r;
+    };
+    uint16_t lr;
+} reg8_pair;
+
+typedef struct {
+    reg8_pair IR_IE;
+    reg8_pair AF;
+    reg8_pair BC;
+    reg8_pair DE;
+    reg8_pair HL;
+    uint16_t PC;
+    uint16_t SP;
+} register_file;
+
+extern register_file rf;
 
 #endif // CPU_H
--- src/cpu.c
+++ src/cpu.c
@@ -1,5 +1,22 @@
 #include "cpu.h"
+#include <stdint.h>
+#include "raylib.h"
 
+register_file rf;
+
+int state = 0;
 void clock_cpu() {
     // TODO: CPU functionality
+    switch (state) {
+        case 0:
+            rf.AF.l = (uint8_t)42;
+            break;
+        case 1:
+            rf.AF.r = (uint8_t)42;
+            break;
+        case 10:
+            TraceLog(LOG_INFO, "Reg A: %d, Reg F: %d, AF: %d", rf.AF.l, rf.AF.r, rf.AF.lr);
+            break;
+    }
+    state++;
 }
