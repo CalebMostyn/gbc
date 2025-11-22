fe09f3e9af2ba438e181ab7f8c4d3f0b9f22a020
CalebMostyn <calebmostyn@gmail.com>
2025-11-21 21:13:09 -0500

    Simplifying Build Local Script

--- build-local.sh
+++ build-local.sh
@@ -4,16 +4,11 @@
 IMAGE_NAME="gbc-build:test"
 CONTAINER_NAME="gbc-build-running"
 
-BUILD_COMMANDS="
-cd workspace/
-make
-"
-
 echo "=== Running container: $CONTAINER_NAME ==="
 docker run --rm -dit --name "$CONTAINER_NAME" -v "$PWD":/workspace "$IMAGE_NAME" bash
 
 echo "=== Building inside container ==="
-docker exec -it "$CONTAINER_NAME" bash -c "$BUILD_COMMANDS"
+docker exec -it "$CONTAINER_NAME" bash -c "cd workspace && chmod +x build.sh && ./build.sh"
 
 echo "=== Stopping container ==="
 docker stop "$CONTAINER_NAME" >/dev/null &
