39fdd529065a1e0d746e770de6115ef82a4a9a0d
CalebMostyn <calebmostyn@gmail.com>
2026-08-25 22:43:02 -0500

    Improving Initial Tests

--- tests/test_cpu.c
+++ tests/test_cpu.c
@@ -1,29 +1,62 @@
 #include "munit.h"
 #include "cpu.h"
 
-static MunitResult test_pc_increment() {
-    munit_assert_int(rf.PC, ==, 0);
-    clock_cpu();
-    munit_assert_int(rf.PC, ==, 1);
-    return MUNIT_OK;
+static void assert_register_file_equal(register_file a, register_file b) {
+    munit_assert_int(a.IME, ==, b.IME);
+    munit_assert_int(a.AF.lr, ==, b.AF.lr);
+    munit_assert_int(a.BC.lr, ==, b.BC.lr);
+    munit_assert_int(a.DE.lr, ==, b.DE.lr);
+    munit_assert_int(a.HL.lr, ==, b.HL.lr);
+    munit_assert_int(a.PC, ==, b.PC);
+    munit_assert_int(a.SP, ==, b.SP);
 }
 
+static MunitResult test_pc_increment() {
+    munit_assert_int(rf.PC, ==, 0);
+    // each clock pulse should increment PC,
+    // as memory is initialized to all 0s (NOP)
+    // which only take 1 clock cycle
+    for (int i = 1; i <= 1000; i++) {
+        clock_cpu();
+        munit_assert_int(rf.PC, ==, i);
+    }
+    // Assert the only register updated was PC
+    register_file blank_rf;
+    memset(&blank_rf, 0, sizeof(blank_rf));
+    blank_rf.PC = 1000;
+    assert_register_file_equal(blank_rf, rf);
+    return MUNIT_OK;
+}
 static MunitResult test_cpu_halt() {
+    // set first instruction to CPU halt
     memory[0] = 0x76;
     munit_assert_int(rf.PC, ==, 0);
+    // first clock pulse, fetches instruction at mem 0
     clock_cpu();
     munit_assert_int(rf.PC, ==, 1);
-
+    // second clock pulse, executes CPU halt
     clock_cpu();
     munit_assert_int(rf.PC, ==, 2);
     munit_assert_true(cpu_halted);
 
+    rf.IME = 1; // interrupts being enabled should not impact halt as mem is 0'd out
+    register_file old_rf = rf;
+    // PC should no longer increment, instructions should not be executed
+    for (int i = 1; i <= 1000; i++) {
+        clock_cpu();
+        munit_assert_int(rf.PC, ==, 2);
+        assert_register_file_equal(old_rf, rf);
+    }
+
+    // Force an interrupt, which should resume CPU on next clock pulse
+    memory[IE_ADDR] = 0x01;
+    memory[IF_ADDR] = 0x01;
     clock_cpu();
-    munit_assert_int(rf.PC, ==, 2);
-    clock_cpu();
-    munit_assert_int(rf.PC, ==, 2);
-    clock_cpu();
-    munit_assert_int(rf.PC, ==, 2);
+    munit_assert_false(cpu_halted);
+    // PC to ISR location
+    munit_assert_int(rf.PC, ==, 64);
+    // TODO: Validate halt bug where cpu is resumed if there is a pending
+    // interrupt when interrupts are disabled
     return MUNIT_OK;
 }
 
